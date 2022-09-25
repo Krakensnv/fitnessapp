9d230023b195c4c20faadcc17fa19a78ac4d6e10
roman-kojnok <roman.kojnok@protonmail.com>
2022-09-25 14:48:17 +0100

    first commit

---
+++ main/blueprints/page/views.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+from flask import Blueprint, render_template
+
+page = Blueprint('page', __name__, template_folder='templates')
+calc = Blueprint('calc', __name__, template_folder='templates')
+
+
+@page.route('/')
+def home():
+    return render_template('page/home.html')
+
+
+@page.route('/terms')
+def terms():
+    return render_template('page/terms.html')
+
+
+@page.route('/privacy')
+def privacy():
+    return render_template('page/privacy.html')
+
+
+@page.route('/FAQ')
+def FAQ():
+    return render_template('page/FAQ.html')
+
+
+@calc.route('/calculator')
+def calculator():
+    return render_template('calc/calculator.html')
